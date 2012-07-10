2cae873fbc32513870c1d22f3ac154304c440d2e
jahande <r.jahande@gmail.com>
2012-07-10 23:12:10 +0430

    Main Function added

---
+++ src/controllers/Main.java
@@ -0,0 +1,15 @@
+package controllers;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Controller c = new SimpleController();
+		c.next(null, null, "start");
+
+	}
+
+}
